4623ae5ccbef8b0177e64b75951375b994daa92f
melmar12 <melmar12@live.com>
2016-09-13 17:53:57 +0000

    removed file

--- names-dob.rb
+++ names-dob.rb
@@ -1,10 +1,10 @@
-# filename = ARGV.first
-
  puts "Hello, what is your name?"
  name = gets.chomp
  puts "What is your DOB?"
  dob = gets.chomp
 
 open('directory.txt', 'w') do |f|
-  f.puts "name: " + name + " DOB: " + dob
+
+  f.write "name: " + name + " DOB: " + dob
+
 end
